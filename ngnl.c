78428072b0529b8d6b70bd40209424829ac983bc
kmaputla <kmaputla@student.wethinkcode.co.za>
2018-06-10 14:56:51 +0200

    add

--- ngnl.c
+++ ngnl.c
@@ -6,7 +6,7 @@
 /*   By: kmaputla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/09 11:37:28 by kmaputla          #+#    #+#             */
-/*   Updated: 2018/06/09 17:25:41 by kmaputla         ###   ########.fr       */
+/*   Updated: 2018/06/10 14:51:52 by kmaputla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int				get_next_line(int fd, char **line)
 	char		buf[1 + BUFF_SIZE];
 	int			size;
 	int			i;
+	char		*swap;
 	char		*rtn;
 
 	i = 1;
@@ -31,18 +32,22 @@ int				get_next_line(int fd, char **line)
 		{
 			ft_bzero(buf, 1 + BUFF_SIZE);
 			size = read(fd, buf, BUFF_SIZE);
-			rtn = ft_strjoin(rtn, buf);
+			swap = rtn;
+			rtn = ft_strjoin(swap, buf);
 		}
 		else
 		{
 			rtn = head;
 			head = NULL;
 		}
-		if ((head = ft_strdup(ft_strchr(rtn, '\n') + 1)))
+		if ((head = ft_strchr(rtn, '\n')))
 		{
-			ft_strclr(ft_strchr(rtn, '\n') - 1);
+			head = ft_strdup(head + 1);
+			ft_strclr(ft_strchr(rtn, '\n'));
 			i = 0;
 		}
+		if (size == 0)
+			i = 0;
 	}
 	(*line) = rtn;
 	return (size);
