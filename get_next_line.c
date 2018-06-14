a0ff4e42cd7d0de5902e5d939fb00a7f1b182ce0
kmaputla <kmaputla@student.wethinkcode.co.za>
2018-06-14 18:13:57 +0200

    new file:   author 	modified:   get_next_line.c 	modified:   get_next_line.h 	deleted:    gnl.c 	deleted:    ngnl.c 	deleted:    test.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,77 +6,73 @@
 /*   By: kmaputla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/11 14:08:47 by kmaputla          #+#    #+#             */
-/*   Updated: 2018/06/13 17:11:11 by kmaputla         ###   ########.fr       */
+/*   Updated: 2018/06/14 11:11:31 by kmaputla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-t_list	*search(t_list **str, size_t fd)
+static	t_list	*search(t_list **str, size_t fd)
 {
 	t_list *run;
-	t_list *hold;
 
 	run = (*str);
-	hold = NULL;
 	while (run != NULL && run->content_size != fd)
 	{
-		hold = run;
 		run = run->next;
 	}
 	if (run == NULL)
 	{
-		ft_lstadd(str, ft_lstnew(0, 0));
-		(*str)->content_size  = fd;
+		ft_lstadd(str, ft_lstnew(0, fd));
 		return (*str);
 	}
-	else if (run->content_size == fd)
-		return (run);
-	return (0);
+	return (run);
 }
 
-static	void	join(char **line, char *buf)
+static	int		join(t_list *hold, int fd, char **line)
 {
-	char *temp;
+	char	*temp;
+	char	buf[1 + BUFF_SIZE];
+	int		size;
 
-	temp = 0;
-	if ((*line) == NULL)
-	   (*line) = ft_strdup(buf);
+	size = 1;
+	temp = NULL;
+	if (hold->content == NULL)
+	{
+		ft_bzero(buf, BUFF_SIZE);
+		size = read(fd, buf, BUFF_SIZE);
+		if ((*line) == NULL)
+			(*line) = ft_strdup(buf);
+		else
+		{
+			temp = (*line);
+			(*line) = ft_strjoin((*line), buf);
+			free(temp);
+		}
+	}
 	else
 	{
-		temp = (*line);
-		(*line) = ft_strjoin((*line), buf);
-		free(temp);
+		(*line) = hold->content;
 	}
+	return (size);
 }
 
-int	get_next_line(int fd, char **line)
+int				get_next_line(int fd, char **line)
 {
 	static	t_list	*save;
-	char			buf[1 + BUFF_SIZE];
 	t_list			*hold;
 	int				size;
 	int				i;
 
 	i = 1;
 	size = 1;
-	if (fd < 0|| !line)
+	if (fd < 0 || !line)
 		return (-1);
 	(*line) = NULL;
 	hold = search(&save, (size_t)fd);
 	while (i == 1 && size > 0)
 	{
-		if (hold->content == NULL)
-		{
-			ft_bzero(buf, BUFF_SIZE);
-			size = read(fd, buf, BUFF_SIZE);
-			join(line, buf);
-		}
-		else
-		{
-			(*line) = hold->content;
-			free(hold->content = NULL);
-		}
+		size = join(hold, fd, line);
 		if ((hold->content = ft_strchr((*line), '\n')))
 		{
 			hold->content = ft_strdup(hold->content + 1);
@@ -84,8 +80,7 @@ int	get_next_line(int fd, char **line)
 			i = 0;
 		}
 	}
-	if (size > 1)
+	if (size > 1 || ft_strlen(*line))
 		size = 1;
 	return (size);
 }
-
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: kmaputla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/30 08:57:21 by kmaputla          #+#    #+#             */
-/*   Updated: 2018/06/12 14:09:36 by kmaputla         ###   ########.fr       */
+/*   Updated: 2018/06/13 11:24:21 by kmaputla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 # define GET_NEXT_LINE_H
 # include <unistd.h>
 # include <stdlib.h>
-# define BUFF_SIZE 100
+# include "./libft/libft.h"
+# define BUFF_SIZE 1
 
 int	get_next_line(int fd, char **line);
 #endif
